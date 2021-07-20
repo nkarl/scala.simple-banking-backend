92868132f9aa91e6d718f30af7ba35faeb7d037f
Karl N <khoa.norden@gmail.com>
2021-07-19 17:11:29 -0700

    laid out case matching template for the menu loop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,8 @@
 import java.util.Scanner
-import engine.{Menu}
+import engine.{Menu, Session}
 
 @main def hello: Unit = 
-  Menu.mockCLI
-  Menu.printMenu
+  //Menu.mockCLI
+  var session = Session()
+  session.runSession()
+
--- src/main/scala/engine/Session.scala
+++ src/main/scala/engine/Session.scala
@@ -1,16 +1,15 @@
-package engine.Session
+package engine
 
+import scala.io.StdIn
 import engine.Menu
 
 class Session:
-  def runSession(run: Boolean): Unit = run match
-    case false =>
-      return
-    case true =>
-      var i: Int = 0
-      while 
-        i < 10
-      do
-        i = i + 1
-        println("session is running...")
-      println("session stopped.")
+  def runSession(): Unit =
+    Menu.printMenu
+    while true do
+      var op = StdIn.readInt
+      op match
+        case 0 => return
+        case 1 => println("Creating new account...")
+        case 2 => println("Logging into existing account...")
+        case _ => print("Wrong input, choose again: ")
