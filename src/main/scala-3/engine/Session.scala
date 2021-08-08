8bac13c1b2ec9a5e0925857b3c8b0a367dd903b8
nkarl <nordenk@pm.me>
2021-08-08 10:23:29 -0700

    update

--- src/main/scala-3/engine/Session.scala
+++ src/main/scala-3/engine/Session.scala
@@ -2,25 +2,28 @@ package engine
 
 import scala.io.StdIn
 import engine.Menu
+import scala.annotation.tailrec
 
 class Session:
   def run(): Unit =
     Menu.printMenu
     op(StdIn.readInt)
 
-  def op(i: Int): Unit = i match
-    case 0 => println("Exiting program...")
-    case _ => i match
-      case 1 => println("Creating new account...")
-      case 2 => println("Logging into existing account...")
-      case _ => print("Wrong input, choose again: "); Menu.printMenu
-      op(StdIn.readInt)
+  private def op(i: Int): Unit =
+    var op = (i: Int) => i  // lambda on the fly
+    while true do
+      op(StdIn.readInt) match
+        case 0 => println("Exiting program..."); return
+        case 1 => println("Creating new account...")
+        case 2 => println("Logging into existing account...")
+        case _ => print("Wrong input, choose again: ")
 
-    //var op = (i: Int) => i
-    //while true do
-      //op(StdIn.readInt) match
-        //case 0 => println("Exiting program..."); return
-        //case 1 => println("Creating new account...")
-        //case 2 => println("Logging into existing account...")
-        //case _ => print("Wrong input, choose again: ")
+  //@tailrec
+  //private def op(i: Int): Unit = i match
+    //case 0 => println("Exiting program...")
+    //case _ => i match
+      //case 1 => println("Creating new account...")
+      //case 2 => println("Logging into existing account...")
+      //case _ => print("Wrong input, choose again: "); Menu.printMenu
+      //op(StdIn.readInt)
 
